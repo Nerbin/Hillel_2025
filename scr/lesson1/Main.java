0d3ab0fa8ffd60f27cd816fec74c652d00cd37cc
Ivan <mokrakivan17@gmail.com>
2024-12-26 01:11:31 +0200

    adding functionality

--- scr/lesson1/Main.java
+++ scr/lesson1/Main.java
@@ -6,16 +6,25 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Convecter App.");
-        System.out.println("App for measures converting");
+        System.out.println("Version 1.0");
+
 
 
         double kgs = 5;
+        double pnds = 12;
         double pounds = convKgsToPounds(kgs);
-        System.out.println("Result is "+pounds+" pounds.");
+        double kilos = convPoundsToKgs(pnds);
+        System.out.println("Result is "+pounds+" pounds and "+ kilos+ "kgs");
 
     }
 
     private static double convKgsToPounds(double kgs){
+
         return kgs*CONV_K;
+    }
+
+    private static double convPoundsToKgs(double pnds){
+
+        return pnds/CONV_K;
     }
 }
