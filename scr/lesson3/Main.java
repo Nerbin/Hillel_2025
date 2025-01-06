7f5b25b728ce044be5d4b6c854b452ccd170bdfc
Ivan <mokrakivan17@gmail.com>
2025-01-06 03:42:30 +0200

    Celsius to Fahrenheit conversion

--- scr/lesson3/Main.java
+++ scr/lesson3/Main.java
@@ -4,10 +4,17 @@ public class Main {
     public static double fahrenheitToCelsius(double fahrenheit) {
         return (fahrenheit - 32) * 5 / 9;
     }
+    public static double celsiusToFahrenheit(double celsius) {
+        return celsius * 9 / 5 + 32;
+    }
 
     public static void main(String[] args) {
         double fahrenheit = 77.0;
         double celsius = fahrenheitToCelsius(fahrenheit);
         System.out.println("Fahrenheit: " + fahrenheit + " -> Celsius: " + celsius);
+
+        celsius = 25.0;
+        fahrenheit = celsiusToFahrenheit(celsius);
+        System.out.println("Celsius: " + celsius + " -> Fahrenheit: " + fahrenheit);
     }
 }
